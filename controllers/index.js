37a96448c1e2b2b07ae665dd7204e523dc864fd7
Cristopher Moreno Palmer <cmorenop@my.centennialcollege.ca>
2022-02-03 12:12:10 -0500

    Adding - indexController

---
+++ controllers/index.js
@@ -0,0 +1,29 @@
+exports.home = (req, res, next) => {
+    res.render('home', {
+        title: 'Home'
+    });
+}
+
+exports.about = (req, res, next) => {
+    res.render('about', {
+        title: 'About'
+    });
+}
+
+exports.projects = (req, res, next) => {
+    res.render('projects', {
+        title: 'Projects'
+    });
+}
+
+exports.services = (req, res, next) => {
+    res.render('services', {
+        title: 'Services'
+    });
+}
+
+exports.contact = (req, res, next) => {
+    res.render('contact', {
+        title: 'Contact'
+    });
+}
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,20 @@
 var express = require('express');
 var router = express.Router();
+let indexController = require('../controllers/index')
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('home', { title: 'Home' });
-});
+router.get('/', indexController.home);
+
 /* GET about */
-router.get('/about', function (req, res, next) {
-  res.render('about', { title: 'About' });
-});
+router.get('/about', indexController.about);
+
 /* GET project page. */
-router.get('/projects', function (req, res, next) {
-  res.render('projects', { title: 'Projects' });
-});
+router.get('/projects', indexController.projects);
+
 /* GET services page. */
-router.get('/services', function (req, res, next) {
-  res.render('services', { title: 'Services' });
-});
+router.get('/services', indexController.services);
+
 /* GET contact page. */
-router.get('/contact', function (req, res, next) {
-  res.render('contact', { title: 'Contact' });
-});
+router.get('/contact', indexController.contact);
 
 module.exports = router;
