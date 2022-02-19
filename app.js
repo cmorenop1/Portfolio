23928ae00a8fd244a4cc15c9b49b97eff9d54c7e
Cristopher Moreno Palmer <cmorenop@my.centennialcollege.ca>
2022-02-19 14:36:58 -0500

    saving

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -36,6 +36,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+
+  console.error(err)
+
+
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,128 @@
 var express = require('express');
 var router = express.Router();
-let indexController = require('../controllers/index')
 
-/* GET home page. */
-router.get('/', indexController.home);
+let mongoose = require('mongoose');
 
-/* GET about */
-router.get('/about', indexController.about);
 
-/* GET project page. */
-router.get('/projects', indexController.projects);
+//mongodb+srv://dev:<password>@dbportfolio.fhgix.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
-/* GET services page. */
-router.get('/services', indexController.services);
+mongoose.connect("mongodb://localhost:27017/myDB", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
 
-/* GET contact page. */
-router.get('/contact', indexController.contact);
+
+
+
+
+let crypto = require('crypto');
+
+
+// Create a model class
+let UserSchema = mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: {
+        type: String,
+        //match: [/.+\@.+\..+/, "Please fill a valid e-mail address"]
+    },
+    username: {
+        type: String,
+        //unique: true,
+        //required: 'Username is required',
+        trim: true
+    },
+    password: {
+        type: String,
+        validate: [(password) => {
+            return password && password.length > 0;
+        }, 'Password should be longer']
+    },
+    salt: String,
+    created: {
+        type: Date,
+        default: Date.now
+    }
+}, {
+    collection: "user"
+});
+
+
+
+// let indexController = require('../controllers/index')
+//GET METHODS
+//router.get('/', indexController.home);
+// router.get('/about', indexController.about);
+//  router.get('/projects', indexController.projects);
+//  router.get('/services', indexController.services);
+//  router.get('/contact', indexController.contact);
+
+
+router.get('/', (req, res) => {
+
+    res.render('home', {
+        title: 'Home'
+    })
+
+});
+
+router.get('/about', (req, res) => {
+
+    res.render('about', {
+        title: 'About'
+    })
+
+});
+
+router.get('/projects', (req, res) => {
+
+    res.render('projects', {
+        title: 'Projects'
+    })
+
+});
+
+router.get('/services', (req, res) => {
+
+    res.render('services', {
+        title: 'Services'
+    })
+
+});
+
+router.get('/services', (req, res) => {
+
+    res.render('services', {
+        title: 'Services'
+    })
+
+});
+
+router.get('/contact', (req, res) => {
+
+    res.render('contact', {
+        title: 'Contact'
+    })
+
+});
+
+router.get('/register', (req, res) => {
+
+    res.render('register.ejs')
+
+});
+
+router.get('/login', (req, res) => {
+
+    res.render('login.ejs')
+
+});
+
+
+
+//POST
+
+
+
 
 module.exports = router;
